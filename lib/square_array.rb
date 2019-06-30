0fe740163f55c8aa839cfa6dbd06f66e56629d5c
Victoria Cushing <victoriaccushing@gmail.com>
2019-06-30 05:51:48 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,7 +1,7 @@
 def square_array(numbers)
   counter = 0
   arr_length = numbers.length
-  while counter < (arr_length -1) do
+  while counter < arr_length do
     numbers[counter] = numbers[counter]**2
     counter += 1
   end
